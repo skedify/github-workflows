2ab608af270e2f90ccc9c2a4262ad4ae67475273
Simon Van den Broeck <simonvdbroeck@gmail.com>
2021-11-03 22:42:08 +0100

    pure branch?

--- src/utils.ts
+++ src/utils.ts
@@ -97,7 +97,7 @@ export function createOctokitInstance({octokit, repo}: {octokit: OctokitInstance
         owner,
         repo,
         workflow_id: workflowName,
-        ref: `heads/${branchName}`
+        ref: `${branchName}`
       }
     )
   }
