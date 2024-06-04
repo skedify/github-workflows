f835b2bf222fe4a2b5df3b1fb286db7508f1c809
ThomasB <thomas.buys45@gmail.com>
2024-06-04 19:06:48 +0200

    add aggregate-release-notes workflow
    Co-authored-by: Florian Van Damme <vandammeflorian@gmail.com>
    
    Co-authored-by: Demian Dekoninck <DemianD@users.noreply.github.com>

---
+++ src/aggregate-release-notes/createOrUpdateFile.ts
@@ -0,0 +1,49 @@
+import { octokit } from "./main";
+
+async function createFile(
+  rawParams: Parameters<typeof octokit.rest.repos.createOrUpdateFileContents>[0]
+) {
+  const params = rawParams!;
+
+  return octokit.rest.repos.createOrUpdateFileContents(params);
+}
+
+async function updateFile(
+  file: Awaited<ReturnType<typeof octokit.rest.repos.getContent>>,
+  rawParams: Parameters<typeof octokit.rest.repos.createOrUpdateFileContents>[0]
+) {
+  const params = rawParams!;
+
+  return octokit.rest.repos.createOrUpdateFileContents({
+    ...params,
+    // @ts-expect-error Octokit saus
+    sha: file.data.sha,
+  });
+}
+
+export async function createOrUpdateFile(rawParams: Parameters<typeof octokit.rest.repos.createOrUpdateFileContents>[0]) {
+  const params = rawParams!;
+  let file;
+
+  try {
+    file = await octokit.rest.repos.getContent({
+      owner: params.owner,
+      repo: params.repo,
+      ref: `heads/${params.branch}`,
+      path: `${params.path}`,
+    });
+  } catch (error) {
+    // @ts-ignore
+    if (error.status === 404) {
+      // Do nothing, create the file below
+    } else {
+      throw error;
+    }
+  }
+
+  if (file == null) {
+    return createFile(params);
+  }
+
+  return updateFile(file, params);
+}
