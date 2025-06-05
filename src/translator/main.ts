ac1fc969a48ae0adff4b4740781a783be9e8dfde
Simon VDB <simonvdbroeck@gmail.com>
2025-06-05 18:19:18 +0200

    add repo as input as well

--- src/translator/main.ts
+++ src/translator/main.ts
@@ -5,19 +5,19 @@ import { translator } from "./translator";
 
 const mainBranch = core.getInput("mainBranch", { required: true });
 const prBranch = core.getInput("prBranch", { required: true });
+const repo = core.getInput("repo", { required: true });
 
 const envs = z
   .object({
     GITHUB_TOKEN: z.string().min(1),
     GEMINI_API_TOKEN: z.string().min(1),
-    GITHUB_REPOSITORY: z.string().transform((val) => val.split("/")[1]),
   })
   .parse(process.env);
 
 translator({
   mainBranch,
   prBranch,
-  repo: envs.GITHUB_REPOSITORY,
+  repo,
   GITHUB_TOKEN: envs.GITHUB_TOKEN,
   GEMINI_API_TOKEN: envs.GEMINI_API_TOKEN,
 }).catch((err) => {
